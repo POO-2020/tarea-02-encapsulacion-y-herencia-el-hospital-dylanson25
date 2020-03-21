6eb94ea2d5576da97e8f1a76c7b306f5a3b753e5
dylanson25 <dvillarreal0@ucol.mx>
2020-03-20 19:54:56 -0600

    avanxe

---
+++ asegurado.js
@@ -0,0 +1,12 @@
+import Paciente from './paciente.js'
+export default class Asegurado extends paciene{
+    constructor({numeroPoliza, finVigencia, compañia}){
+        super(nombre, fechaNacimiento, telefono)
+        this._numeroPoliza = numeroPoliza
+        this._finVigencia = finVigencia
+        this._compañia = compañia 
+    }
+    getPerfil(){
+        return``
+    }
+}
